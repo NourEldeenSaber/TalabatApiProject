615082895d1b4193942f457db12855e467439185
NourSaber <nour01saber@gmail.com>
2025-10-25 18:28:24 +0300

    Add project files.

---
+++ TalabatDemo/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TalabatDemo.Models;
+
+namespace TalabatDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase // BaseUrl/Api/ControllerName[Product]
+    {
+        [HttpGet("{id}")] // GET : BaseUrl/Api/ControllerName[Product]
+        public ActionResult<Product> Get(int id)
+        {
+            return new Product() { Id = id };
+        }
+
+        [HttpGet]
+        public ActionResult<Product> GetAll()
+        {
+            return new Product() { Id = 20 };
+        }
+
+        [HttpPost]
+        public ActionResult<Product> AddProduct(Product product)
+        {
+            return new Product();
+        }
+
+        [HttpPost("brand")]
+        public ActionResult<Product> AddBrand(Product product)
+        {
+            return new Product();
+        }
+
+        [HttpPut]
+        public ActionResult<Product> UpdateProduct(Product product)
+        {
+            return new Product();
+        }
+
+        [HttpDelete]
+        public ActionResult<Product> DeleteProduct(Product product)
+        {
+            return new Product();
+        }
+
+
+    }
+}
