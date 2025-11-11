d8d084372dc87f6f18ff1a6e99dc23901491a93a
NourSaber <nour01saber@gmail.com>
2025-11-11 14:06:32 +0200

    IproductService-ProductDto

---
+++ Core/ServiceAbstractionLayer/IProductService.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceAbstractionLayer
+{
+    public class IProductService
+    {
+        // Get All Products
+
+        // Get Product By Id
+
+        // Get All Types
+
+        // Get All Brands
+    }
+}
